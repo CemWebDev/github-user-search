01194244305987877a16bb6e20e776b4ba873eb8
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-17 00:39:50 +0300

    creating data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+const userData = [
+  { label: "Name", key: "name" },
+  { label: "Username", key: "login" },
+  { label: "Public Repositories", key: "public_repos" },
+  { label: "Followers", key: "followers" },
+  { label: "Following", key: "following" },
+  { label: "Avatar", key: "avatar_url" },
+];
