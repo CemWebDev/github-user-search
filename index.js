357f78ba90b7dbd9cd3be57f221db6b3a59d1078
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-17 00:17:22 +0300

    starting javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+const searchBtn = document.querySelector(".search-btn");
+
+const searchUser = () => {
+  const username = document.querySelector("#username-input").value;
+  fetch(`https://api.github.com/users/${username}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching user data", error);
+    });
+};
+
+searchBtn.addEventListener("click", searchUser);
