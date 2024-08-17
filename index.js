dc5f1e4cde738c71776483532c3f15d808cfd318
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-17 19:01:11 +0300

    new commit

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ const searchUser = () => {
   fetch(`https://api.github.com/users/${username}`)
     .then((response) => response.json())
     .then((data) => {
-      githubProfile.innerHTML = "";
+      Array.from(document.getElementsByClassName("user-data-item")).forEach(
+        (element) => {
+          element.innerHTML = "";
+        }
+      );
 
       userData.forEach((item) => {
         const value = data[item.key];
-        const element = document.createElement("div");
-        element.classList.add("user-data-item");
+        const element = document.getElementById(item.key);
 
-        if (item.key === "avatar_url") {
-          const img = document.createElement("img");
-          img.src = value;
-          img.alt = `${data.login}'s avatar`;
-          img.classList.add("avatar")
-          element.appendChild(img);
+        if (element) {
+          if (item.key === "avatar_url") {
+            const img = document.createElement("img");
+            img.src = value;
+            img.alt = `${data.login}'s avatar`;
+            img.classList.add("avatar");
+            element.appendChild(img);
+          } else if (item.key === "login") {
+            element.innerHTML = `<h2>${value}</h2>`;
+          } else {
+            element.innerHTML = `${item.label}: ${value}`;
+          }
         } else {
-          element.innerHTML = `<div> ${item.label}: ${value} </div>`;
+          console.error(`Element with id ${item.key} not found.`);
         }
-
-        githubProfile.appendChild(element);
       });
     })
     .catch((error) => {
