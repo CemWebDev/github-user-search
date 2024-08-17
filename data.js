3ac5836ff2efd6c0ebe054b9a249373e1c3f1033
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-17 18:19:41 +0300

    updated data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const userData = [
+  { label: "Avatar", key: "avatar_url" },
   { label: "Username", key: "login" },
   { label: "Public Repositories", key: "public_repos" },
   { label: "Followers", key: "followers" },
   { label: "Following", key: "following" },
-  { label: "Avatar", key: "avatar_url" },
 ];
